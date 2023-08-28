c2af4ecded49c8b4f31c2c555e2e036e7c09f15f
Mohammed el habib Ouis <123908808+ouis31@users.noreply.github.com>
2023-08-28 22:21:25 +0200

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,19 @@
-#include "holberton.h"
-/**
- * set_string - function that  sets the value of a pointer to a char.
- *@s: first value -char
- *@to: second value -int
- */
-void set_string(char **s, char *to)
-{
+#include "main.h"
+#include <stdio.h>
 
-	*s = to;
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s0 = "Bob Dylan";
+    char *s1 = "Robert Allen";
+
+    printf("%s, %s\n", s0, s1);
+    set_string(&s1, s0);
+    printf("%s, %s\n", s0, s1);
+    return (0);
 }
 
